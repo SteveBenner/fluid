6f6aee06587bbd70b21f9d32a2772440c9c8b251
Stephen Benner <SteveBenner@users.noreply.github.com>
2014-04-23 06:55:24 -0400

    remove personal info

--- lib/hydronamic.rb
+++ lib/hydronamic.rb
@@ -2,7 +2,7 @@ require 'hydronamic/version'
 Bundler.require :default
 
 module Hydronamic
-	APP_DIR = '/Users/neo/Library/Application Support/Fluid/FluidApps'
+	APP_DIR = "#{ENV['HOME']}/Library/Application Support/Fluid/FluidApps"
 
 	# todo: method - update all apps
 end
